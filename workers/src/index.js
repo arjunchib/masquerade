2eaee0634b8887da99c1b35c9ad6b388f3048400
Arjun Chib <arjunpatrickchib@gmail.com>
2022-01-16 03:56:00 -0500

    Split projects into two projects

--- workers/src/index.js
+++ workers/src/index.js
@@ -1,18 +1,16 @@
+export { GameRoom } from "./GameRoom";
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     let path = url.pathname.slice(1).split("/");
 
-    if (!path[0]) {
-      return env.ASSETS.fetch(request);
-    }
-
     switch (path[0]) {
       case ".functions":
         return handleApiRequest(path.slice(1), request, env);
       default:
-        return env.ASSETS.fetch(request);
+        return new Response("Not Found", { status: 404 });
     }
   },
 };
@@ -46,38 +44,5 @@ async function handleApiRequest(path, request, env) {
     }
     default:
       return new Response("Not found", { status: 404 });
-  }
-}
-
-export class GameRoom {
-  constructor(controller, env) {
-    this.sessions = [];
-  }
-
-  async fetch(request) {
-    let url = new URL(request.url);
-
-    switch (url.pathname) {
-      case "/websocket": {
-        // The request is to `/api/room/<name>/websocket`. A client is trying to establish a new WebSocket session.
-        if (request.headers.get("Upgrade") !== "websocket") {
-          return new Response("expected websocket", { status: 400 });
-        }
-        // eslint-disable-next-line no-undef
-        let pair = new WebSocketPair();
-        await this.handleSession(pair[1]);
-        return new Response(null, { status: 101, webSocket: pair[0] });
-      }
-      default:
-        return new Response("Not found", { status: 404 });
-    }
-  }
-
-  async handleSession(webSocket) {
-    webSocket.accept();
-    webSocket.addEventListener("message", (event) => {
-      console.log(event.data);
-      webSocket.send("PONG");
-    });
   }
 }
