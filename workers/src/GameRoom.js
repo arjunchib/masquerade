b4b870a2db7024adae25b40793f8077df7f9406d
Sang Chan Kim <sck970112@gmail.com>
2022-01-16 23:34:42 -0800

    Delete

--- workers/src/GameRoom.js
+++ workers/src/GameRoom.js
@@ -33,33 +33,6 @@ export class GameRoom {
     });
   }
 
-  async fetch(request) {
-    let url = new URL(request.url);
-
-    switch (url.pathname) {
-      case "/websocket": {
-        // The request is to `/api/room/<name>/websocket`. A client is trying to establish a new WebSocket session.
-        if (request.headers.get("Upgrade") !== "websocket") {
-          return new Response("expected websocket", { status: 400 });
-        }
-        // eslint-disable-next-line no-undef
-        let pair = new WebSocketPair();
-        await this.handleSession(pair[1]);
-        return new Response(null, { status: 101, webSocket: pair[0] });
-      }
-      default:
-        return new Response("Not found", { status: 404 });
-    }
-  }
-
-  async handleSession(webSocket) {
-    webSocket.accept();
-    webSocket.addEventListener("message", (event) => {
-      console.log(event.data);
-      webSocket.send("PONG");
-    });
-  }
-
   createSession(webSocket, userID) {
     if (userID in this.sessions) {
       // Close out old web socket connection
