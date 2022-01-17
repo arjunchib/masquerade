5175fa8a3664f8f58329aaeecd7e52cbef6e0953
Sang Chan Kim <sck970112@gmail.com>
2022-01-17 00:11:56 -0800

    Init

--- workers/src/GameRoom.js
+++ workers/src/GameRoom.js
@@ -2,7 +2,9 @@ import { HeartBeatManager } from "./HeartbeatManager";
 
 export class GameRoom {
   constructor(controller, env) {
-    this.sessions = [];
+    this.sessions = {};
+    this.controller = controller;
+    this.env = env;
   }
 
   async fetch(request) {
@@ -34,5 +36,67 @@ export class GameRoom {
         webSocket.send("TEST ACK");
       }
     });
+  }
+
+  createSession(webSocket, userID) {
+    if (userID in this.sessions) {
+      // Close out old web socket connection
+      let session = this.sessions[userID];
+      try {
+        session.webSocket.close();
+      } catch (e) {}
+    }
+
+    newSession = {
+      alive: true,
+      webSocket: webSocket,
+      heartbeatManager: new HeartBeatManager(this, webSocket),
+    };
+
+    webSocket.addEventListener("message", (event) => {
+      try {
+        let eventData = JSON.parse(event.data);
+        if (eventData["type"] === "GameEvent") {
+          this.gameState.processGameEvent(this, eventData);
+  
+        } else if (eventData["type"] === "UiEvent") {
+          this.gameState.processUIEvent(this, eventData);
+        }
+      } catch(e) {}
+    });
+
+    this.sessions[userID] = newSession;
+  }
+
+  startGame() {
+    // Check if we have enough players
+    let aliveSessions = Object.values(this.sessions).filter((x) => x.alive);
+    if (aliveSessions.length < MIN_PLAYER) {
+      throw new NotEnoughPlayers();
+    }
+
+    this.gameState.start();
+  }
+
+  destroy() {
+    this.sessions.forEach((session) => {
+      session.webSocket.close(1000, "Game ended");
+    });
+  }
+
+  getSession(userID) {
+    return this.sessions[userID];
+  }
+
+  markSessionAlive(userID) {
+    if (!this.sessions[userID].alive) {
+      // if previously dead session comes back alive
+      this.sessions[webSocket].send(this.gameState)
+    }
+    this.sessions[userID].alive = true;
+  }
+
+  markSessionDead(userID) {
+    this.session[userID].alive = false;
   }
 }
